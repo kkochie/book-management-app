5c35d48db7e3b4e289186ccc644d5754f3ed23ae
kkochie <kristinekochie@gmail.com>
2023-01-23 11:03:03 -0500

    Display Books on BookList

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,7 +1,27 @@
 import React from 'react'
+import _ from 'lodash'
+import Book from './Book'
 
-const BooksList = () => {
-  return <h2>List of Books</h2>
+const BooksList = ({ books, setBooks }) => {
+  // Call setBooks function with filter() to keep only books that do not match with provided book id
+  const handleRemoveBook = (id) => {
+    setBooks(books.filter((book) => book.id !== id))
+  }
+
+  return (
+    <React.Fragment>
+      <div className="book-list">
+        {/* Loop over books array and pass them as props to Book component */}
+        {!_.isEmpty(books) ? (
+          books.map((book) => (
+            <Book key={book.id} {...book} handleRemoveBook={handleRemoveBook} />
+          ))
+        ) : (
+          <p className="message">No books available. Please add some books.</p>
+        )}
+      </div>
+    </React.Fragment>
+  )
 }
 
 export default BooksList
