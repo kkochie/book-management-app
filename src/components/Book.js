4423c10ffe6962bcfdd051d7364ad4d289e5a9bd
kkochie <kristinekochie@gmail.com>
2023-01-23 20:59:02 -0500

    Add useHistory to Book

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 import { Button, Card } from 'react-bootstrap'
 
 const Book = ({ id, bookname, author, price, quantity, date, handleRemoveBook }) => {
+  const history = useHistory()
+
   return (
     <Card style={{ width: '18rem'}} className="book" >
       <Card.Body>
@@ -12,7 +15,11 @@ const Book = ({ id, bookname, author, price, quantity, date, handleRemoveBook })
           <div>Price: {price}</div>
           <div>Date: {new Date(date).toDateString()}</div>
         </div>
-        <Button variant="primary">Edit</Button>{''}
+        {/* Add onClick handler to redirect user to /edit/id_of_the_book route when clicked */}
+        <Button variant="primary" onClick={() => 
+          history.pushState(`/edit/${id}`)}>
+            Edit
+        </Button>{''}
         <Button variant="danger" onClick={() =>
           handleRemoveBook(id)}>
             Delete
